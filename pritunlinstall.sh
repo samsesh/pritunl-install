04f5499c92bb503b6f2fa8eddc6745b00fc0cfe0
Sam Sesh <admin@samsesh.net>
2022-10-11 22:14:41 +0330

    null

--- pritunlinstall.sh
+++ pritunlinstall.sh
@@ -20,8 +20,13 @@ clear
 
 figlet "install Pritunl on ubuntu 20.04"
 sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
-deb https://repo.pritunl.com/stable/apt-get focal main
+deb http://repo.pritunl.com/stable/apt focal main
 EOF
+
+sudo apt --assume-yes install gnupg
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A | sudo tee /etc/apt/trusted.gpg.d/pritunl.asc
+sudo apt update
 sleep 2
 clear
 
