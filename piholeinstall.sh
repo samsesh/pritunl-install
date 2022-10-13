f519646a3df5f0339005dc8354f170aef96a71e1
127.0.0.1:443 <admin@samsesh.net>
2022-10-13 17:06:49 +0330

    Update piholeinstall.sh

--- piholeinstall.sh
+++ piholeinstall.sh
@@ -3,6 +3,6 @@ clear
 figlet "installing pihole"
 curl -sSL https://install.pi-hole.net | bash
 cd /tmp/pihole-dns/
-cp /tmp/pihole-dns/pihole-dns/external.conf /etc/lighttpd/
+cp /tmp/pihole-dns/pihole-dns/external.conf /etc/lighttpd/external.conf
 cp /tmp/pihole-dns/pihole-dns/pihole-FTL.conf /etc/pihole/pihole-FTL.conf
 service lighttpd restart
