8cd1e149ca923fbf42acce396f2429e721d45328
Sam Sesh <admin@samsesh.net>
2023-04-11 19:03:08 +0330

    clean code

--- pritunlinstall.sh
+++ pritunlinstall.sh
@@ -1,75 +1,109 @@
 #!/bin/bash
-
+check_if_running_as_root() {
+    # If you want to run as another user, please modify $EUID to be owned by this user
+    if [[ "$EUID" -ne '0' ]]; then
+        echo "$(tput setaf 1)Error: You must run this script as root!$(tput sgr0)"
+        exit 1
+    else
+        echo $(tput setaf 2)pritunl installing starting ...$(tput sgr0)
+    fi
+}
+sysup(){
 #update repo silent
-echo "update repository"
-sudo apt-get -qq update >> /dev/null
+bash <(curl -s https://raw.githubusercontent.com/samsesh/Ubuntu-Optimizer/main/ubuntu-optimizer.sh)
+}
+
+req(){
+#install requerment
+sudo apt-get -qq install dialog figlet  python3 -y >> /dev/null
 sleep 2
 clear
-#install figlet
-sudo apt-get -qq install figlet -y >> /dev/null
-sudo apt-get -qq install zip unzip autossh tmux python3 -y >> /dev/null
-sleep 2
-clear
-#install pritunl
-figlet "install Pritunl on ubuntu 20.04"
+}
+
+monogodaddsources(){
 sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list << EOF
 deb https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse
 EOF
 sleep 2
 clear
+}
 
-figlet "install Pritunl on ubuntu 20.04"
+pritunladdsources(){
 sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
 deb http://repo.pritunl.com/stable/apt focal main
 EOF
+}
 
+pritunladdgnupgkey(){
 sudo apt --assume-yes install gnupg
 gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A | sudo tee /etc/apt/trusted.gpg.d/pritunl.asc
 sudo apt update
 sleep 2
 clear
+}
 
-figlet "install Pritunl on ubuntu 20.04"
+mongodaddgnupgkey(){
 sudo apt-get --assume-yes install gnupg >> /dev/null
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 sleep 2
 clear
+}
 
-figlet "install Pritunl on ubuntu 20.04"
+ubuntukeyadd(){
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 sleep 2
 clear
+}
 
-figlet "install Pritunl on ubuntu 20.04"
+updaterepo(){
 figlet "update repository"
 sudo apt-get -qq update >> /dev/null
 sleep 2
 clear
+}
 
-figlet "install Pritunl on ubuntu 20.04"
-figlet "install database and pritunl"
+pritunlinstall(){
+figlet "install pritunl"
 sudo apt-get --assume-yes -qq install pritunl mongodb-org >> /dev/null
 sleep 2
 clear
+}
 
-figlet "install Pritunl on ubuntu 20.04"
-figlet "run database and pritunl"
+mongodinstall(){
+figlet "install database "
+sudo apt-get --assume-yes -qq install  mongodb-org >> /dev/null
+sleep 2
+clear
+}
+
+pritunlservice(){
+echo "run pritunl"
 sudo systemctl start pritunl mongod 
 sleep 2
 clear
+}
 
-figlet "install Pritunl on ubuntu 20.04"
-figlet "add to startup"
-sudo systemctl enable pritunl mongod
+mongodservice(){
+echo "run database"
+sudo systemctl start mongod 
 sleep 2
 clear
+}
 
-#mkdir on temp
-mkdir -p /tmp/pritunlinstall /tmp/pihole-dns 
-cp -r ./pihole-dns /tmp/pihole-dns 
-cp ./piholeinstall.sh /tmp/pihole-dns/piholeinstall.sh 
-cd /tmp
+pritunlstartup(){
+sudo systemctl enable pritunl 
+sleep 2
+clear
+}
+
+mongodstartup(){
+sudo systemctl enable mongod
+sleep 2
+clear
+}
+
+pritunlui(){
 #change ui
 figlet "update web interface"
 cd /tmp/pritunlinstall
@@ -77,37 +111,77 @@ git clone https://github.com/samsesh/pritunl-ui.git ui
 cd ui
 chmod +x update.sh
 sudo bash update.sh 
+}
 
+pritunlcrack(){
 #carck pritunl
-figlet "carck pritunl"
 cd /tmp/pritunlinstall
 apt-get -qq install dialog -y >> /dev/null
 mkdir pritunlfakeapi
 cd pritunlfakeapi
-## for old version
-#wget https://github.com/samsesh/Pritunl-Fake-API/raw/master/server/setup.sh
-#chmod +x setup.sh
-#sudo bash setup.sh
 wget https://raw.githubusercontent.com/samsesh/Pritunl-Fake-API/master/server/setup.py
 chmod +x setup.py
 sudo python3 setup.py --install
-service pritunl restart
-service mongod restart
+}
 
-#update ssh config 
-claer
-figlet "Optimizer system"
-bash <(curl -s https://raw.githubusercontent.com/samsesh/Ubuntu-Optimizer/main/ubuntu-optimizer.sh)
-sleep 5
-clear
-
-
-
-#######
+pritunluse(){
 figlet "your pritunl setup key"
 pritunl setup-key
 echo "you can active your pritunl"
 echo "licens key : active ultimate"
-echo "for close installtion break whit use ctrl + c"
-sleep 360
+echo "Press any key to exit..."
+read -n 1 -s
+echo "Exiting..."
+}
+
+#run
+
+startinstall(){
+    sysup
+    req
+    ubuntukeyadd
+}
+pritunlI(){
+    pritunladdsources
+    pritunladdgnupgkey
+    updaterepo
+    pritunlinstall
+    pritunlui
+    pritunlcrack
+    pritunlservice
+    pritunlstartup
+    pritunluse
+}
+mongodI(){
+    monogodaddsources
+    mongodaddgnupgkey
+    updaterepo
+    mongodinstall
+    mongodservice
+    mongodstartup
+}
+
+
+check_instaled_pritunl(){
+    if command -v pritunl &>/dev/null; then
+        echo $(tput setaf 2)pritunl is already installed on this system.$(tput sgr0)
+        exit 1
+    else
+      startinstall
+      pritunlI
+    fi
+}
+
+check_instaled_mongod(){
+    if command -v mongo &>/dev/null; then
+        echo $(tput setaf 2)MongoDB is already installed on this system.$(tput sgr0)
+        exit 1
+    else
+      startinstall
+      mongodI
+    fi
+}
+check_if_running_as_root
+check_instaled_pritunl
+check_instaled_mongod
 
